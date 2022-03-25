cd8571cf81ec0245d539525072a75063302ddd2a
Chamerli-sh <fares.setbels@gmail.com>
2022-03-25 09:50:36 +0100

    Choice Prompt inquirer

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const inquirer = require('inquirer');
+
 const PL_WBRACK = ['Javascript', 'Java', 'Rust']; // Programming Language with brackets
 const PL_WDP = ['Python'] // Programming Language with the double points
 const PL_CUSED = ['Javascript', 'Java', 'Python']
+
+
+async function askBrackets(){
+    const answer = await inquirer.prompt({
+        name: "brackets",
+        type: "list",
+        message: "Do You Like Brackets?",
+        choices: [
+            "Absolutly, i'd die if not",
+            "Yes I would prefer if",
+            "Don't care",
+            "No I would prefer not",
+            "NOOOO! it 'll kill me"
+        ]
+    });
+
+    return console.log("Hello World!");
+}
+askBrackets();
